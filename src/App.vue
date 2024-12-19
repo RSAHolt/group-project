9686e782954122c9e541fa3bb6a6303a8d1e104c
RSAHolt <lukejsmart@gmail.com>
2024-12-19 04:03:23 +0200

    Updated Background

--- src/App.vue
+++ src/App.vue
@@ -127,10 +127,10 @@ nav a.router-link-exact-active {
   justify-content: center;
   align-items: center;
   height: 100vh;
-  background-image: url(https://www.openaccessgovernment.org/wp-content/uploads/2023/09/iStock-1222540826-scaled.jpg); /* Full viewport height */
+  background-image: url(); /* Full viewport height */
 }
 .back{
-background-image: url("https://media.istockphoto.com/id/1405753437/vector/simple-logo-design.jpg?s=612x612&w=0&k=20&c=Y6vrA9Z4Dl_cqCylROUwp5bWSBQ7BNDgudXRDLCpabI=");
+background-image: url(https://www.openaccessgovernment.org/wp-content/uploads/2023/09/iStock-1222540826-scaled.jpg);
 background-repeat: no-repeat;
 background-size: 100% 100%;
 }
